e5e8814bfb74b8ed27d31f3a956bec35bbe46e17
Nathaniel <nthw.dev@gmail.com>
2021-06-26 16:10:52 -0400

    6/26/21
    started image gallery, product templates. still struggling with connection issues

---
+++ src/templates/ProductTemplate/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+import { Layout, ImageGallery } from 'components';
+import { Grid } from './styles';
+
+export const query = graphql`
+  query ProductQuery($shopifyId: String) {
+    shopifyProduct(shopifyId: { eq: $shopifyId }) {
+      title
+      description
+      images {
+        localFile {
+          childImageSharp {
+            fluid(maxWidth: 200) {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+export default function ProductTemplate(props) {
+  return (
+    <Layout>
+      <Grid>
+        <div>
+          <h1>{props.data.shopifyProduct.title}</h1>
+          <p>{props.data.shopifyProduct.description}</p>
+        </div>
+        <div>
+          <ImageGallery images={props.data.shopifyProduct.images} />
+        </div>
+      </Grid>
+    </Layout>
+  );
+}
